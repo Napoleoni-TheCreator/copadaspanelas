02562131c9aba9ce0fbf5a1b25ed1db71cedaed5
Napoleoni <114572071+Napoleoni-TheCreator@users.noreply.github.com>
2024-04-18 14:27:18 -0300

    comite

--- estagio-luiz/script.js
+++ estagio-luiz/script.js
@@ -1,27 +1,37 @@
   // Função para inicializar o player
-var link_live = window.prompt("Por favor digite o link atual da live","")
-link_live = link_live.replace("https://www.youtube.com/watch?v=","")
 
-  function onYouTubeIframeAPIReady() {
-    // Cria um novo player do YouTube
-    var player = new YT.Player('player', {
-        // ID do vídeo que deseja reproduzir
-        videoId: link_live,
-        // Opções do player
-        playerVars: {
-            // Define a reprodução automática
-            autoplay: 1,
-            // Controles do player
-            controls: 1,
-            // Mostra o título do vídeo
-            showinfo: 1,
-            // Qualidade do vídeo
-            vq: 'hd720'
-        }
-    });
-}
-
-// Carrega a API do YouTube
-window.onload = function() {
-    onYouTubeIframeAPIReady();
-};
+  function link_live(){
+    var link_live = window.prompt("Por favor digite o link atual da live","")
+    link_live = link_live.replace("https://www.youtube.com/watch?v=","")
+    onYouTubeIframeAPIReady(link_live)
+  }
+  
+  
+  document.getElementById("adc_live").addEventListener("click",link_live);
+  
+  
+  
+  
+    function onYouTubeIframeAPIReady(link_live) {
+      // Cria um novo player do YouTube
+      var player = new YT.Player('player', {
+          // ID do vídeo que deseja reproduzir
+          videoId: link_live,
+          // Opções do player
+          playerVars: {
+              // Define a reprodução automática
+              autoplay: 1,
+              // Controles do player
+              controls: 1,
+              // Mostra o título do vídeo
+              showinfo: 1,
+              // Qualidade do vídeo
+              vq: 'hd720'
+          }
+      });
+  }
+  
+  // Carrega a API do YouTube
+  window.onload = function() {
+      onYouTubeIframeAPIReady();
+  };
