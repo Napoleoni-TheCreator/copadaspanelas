0dfa8cc6fc5ec8126020128a8632b1e3b287310d
Kizzo-liso <131163481+Kizzo-liso@users.noreply.github.com>
2024-07-10 14:32:27 -0300

    quase completo

--- app/pages/Jogos Proximos.php
+++ app/pages/Jogos Proximos.php
@@ -306,7 +306,7 @@
                     </div>
                 </blockquote>
                 <script async src="//www.instagram.com/embed.js"></script>
-            </div>
+            </div> <!--
             <div class="last4games" id="game4">
                 <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-permalink="https://www.instagram.com/reel/C4f-6hPOx2P/?utm_source=ig_embed&amp;utm_campaign=loading" data-instgrm-version="14" style=" background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:540px; min-width:326px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);">
                     <div style="padding:16px;"> <a href="https://www.instagram.com/reel/C4f-6hPOx2P/?utm_source=ig_embed&amp;utm_campaign=loading" style=" background:#FFFFFF; line-height:0; padding:0 0; text-align:center; text-decoration:none; width:100%;" target="_blank">
@@ -375,6 +375,7 @@
                 </blockquote>
                 <script async src="//www.instagram.com/embed.js"></script>
             </div>
+    -->
         </section>
 
     </main>
