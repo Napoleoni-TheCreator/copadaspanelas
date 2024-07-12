256c05ed8c7dc20f2c4401d557a5f2a9f9d0d390
thwaverton <113214844+thwaverton@users.noreply.github.com>
2024-07-12 10:09:50 -0300

    Update conexao.php

--- app/config/conexao.php
+++ app/config/conexao.php
@@ -11,5 +11,7 @@ $conn = new mysqli($servername, $username, $password, $database);
 // Verifica a conexão
 if ($conn->connect_error) {
     die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+}else{
+    echo"conexao";
 }
 ?>
