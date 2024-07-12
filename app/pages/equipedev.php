00977ee6e6bbff226bd1180061ba8a8c4f7189b1
Kizzo-liso <131163481+Kizzo-liso@users.noreply.github.com>
2024-07-12 08:15:42 -0300

    Pagina dos Dev do site

---
+++ app/pages/equipedev.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Equipe dev</title>
+    <link rel="stylesheet" href="../../public/css/cssheader.css">
+    <link rel="stylesheet" href="../../public/css/cssfooter.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="../../public/css/devs.css">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <?php include 'header.php' ?>
+
+   
+	<section class="team">
+		<div class="center">
+			<h1>Nosso Time</h1>
+		</div>
+
+		<div class="team-content">
+			<div class="box">
+				<img src="../../public/img/Dev's/thw.jpg">
+				<h3>Thweritons</h3>
+				<h5>Tec. Informatica</h5>
+				<div class="icons">
+					<a href="#"><i class="ri-twitter-fill"></i></a>
+					<a href="#"><i class="ri-facebook-box-fill"></i></a>
+					<a href="#"><i class="ri-instagram-fill"></i></a>
+				</div>
+			</div>
+
+			<div class="box">
+				<img src="../../public/img/Dev's/thw.jpg">
+				<h3>Luiz</h3>
+				<h5>Tec. Informatica</h5>
+				<div class="icons">
+					<a href="#"><i class="ri-twitter-fill"></i></a>
+					<a href="#"><i class="ri-facebook-box-fill"></i></a>
+					<a href="#"><i class="ri-instagram-fill"></i></a>
+				</div>
+			</div>
+
+			<div class="box">
+				<img src="../../public/img/Dev's/thw.jpg">
+				<h3>Eduardo</h3>
+				<h5>Tec. Informatica</h5>
+				<div class="icons">
+					<a href="#"><i class="ri-twitter-fill"></i></a>
+					<a href="#"><i class="ri-facebook-box-fill"></i></a>
+					<a href="#"><i class="ri-instagram-fill"></i></a>
+				</div>
+			</div>
+
+			<div class="box">
+				<img src="../../public/img/Dev's/thw.jpg">
+				<h3>Xaraia</h3>
+				<h5>Tec. Informatica</h5>
+				<div class="icons">
+					<a href="#"><i class="ri-twitter-fill"></i></a>
+					<a href="#"><i class="ri-facebook-box-fill"></i></a>
+					<a href="#"><i class="ri-instagram-fill"></i></a>
+				</div>
+			</div>
+
+            <div class="box">
+				<img src="../../public/img/Dev's/thw.jpg">
+				<h3>riqueles</h3>
+				<h5>Tec. Informatica</h5>
+				<div class="icons">
+					<a href="#"><i class="ri-twitter-fill"></i></a>
+					<a href="#"><i class="ri-facebook-box-fill"></i></a>
+					<a href="#"><i class="ri-instagram-fill"></i></a>
+				</div>
+			</div>
+
+            <div class="box">
+				<img src="../../public/img/Dev's/thw.jpg">
+				<h3>Rafaeu</h3>
+				<h5>Tec. Informatica</h5>
+				<div class="icons">
+					<a href="#"><i class="ri-twitter-fill"></i></a>
+					<a href="#"><i class="ri-facebook-box-fill"></i></a>
+					<a href="#"><i class="ri-instagram-fill"></i></a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+    <?php include 'footer.php' ?>
+</body>
+
+</html>
